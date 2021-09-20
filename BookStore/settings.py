cb439d88e962756c152fb6cbdf80e85590ab71aa
Baxtiyor98 <ergashevb111@gmail.com>
2021-09-20 14:06:18 +0500

    Allow host

--- BookStore/settings.py
+++ BookStore/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-pt$)4ii^+mq#-3u3b16omohi$4r287w1!u=du+8ajegtv=&gti
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
